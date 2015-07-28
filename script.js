c9ed2bfb4c96bf0ee381d19c6d78fa7932c00a5e
Shelley Davis-Wise <shelleydaviswise@gmail.com>
2015-07-28 10:46:17 -0500

    Quiz from 7.28.15 - not feeling it today but I tried.

---
+++ script.js
@@ -0,0 +1,31 @@
+$( document ).ready(function() {
+  // Handler for .ready() called.
+
+// Button event handler
+$("#button").click(("#arraySource").val);
+
+// To catch the array sent from the button
+var $passedArray = $( "#arraySource" ).val();
+  // var passedArray = $( "#arraySource" ).toArray() ;
+  console.log(passedArray);
+
+
+
+
+var newNumArray = [];
+function over25() {
+   for(var i = 0; i < passedArray.length; i++) {
+     if (passedArray[i] > 25) {
+      newNumArray.push;
+    }
+  }
+}
+   var sortedArray = []
+    function arrayToSort(newNumArray){
+    sortedArray = passedArray.sort;
+    return sortedArray;
+  console.log(sortedArray);
+   }
+});
+
+fuck fuck fuck fuck !!!!
