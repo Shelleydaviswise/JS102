c50a5d4c54411661fa73fcfae74c0a4e21a78019
Shelley Davis-Wise <shelleydaviswise@gmail.com>
2015-08-01 12:19:05 -0500

    Refactored code on Aug 2015- fulfills the specifications of the assigment. So there.

--- script.js
+++ script.js
@@ -1,31 +1,51 @@
-$( document ).ready(function() {
-  // Handler for .ready() called.
+var greaterArray = [ ];
 
-// Button event handler
-$("#button").click(("#arraySource").val);
+var outputString = " ";
 
-// To catch the array sent from the button
-var $passedArray = $( "#arraySource" ).val();
-  // var passedArray = $( "#arraySource" ).toArray() ;
-  console.log(passedArray);
+var inputArray = function (arr) {
+    for (var i = 0; i < arr.length; i++) {
+      if (Number(arr[i]) > 25) {
+        greaterArray.push(arr[i]);
 
+      }
+    } 
+  }
+  var sortArr = function (arr){
+    // console.log(arr);
+    return arr.sort();
+  }
 
-
-
-var newNumArray = [];
-function over25() {
-   for(var i = 0; i < passedArray.length; i++) {
-     if (passedArray[i] > 25) {
-      newNumArray.push;
+  var newString = function (arr) {
+    for (var i = 0; i < arr.length; i++) {
+      outputString += "<div class = 'number'>" + arr[i] + "</div>";
     }
   }
-}
-   var sortedArray = []
-    function arrayToSort(newNumArray){
-    sortedArray = passedArray.sort;
-    return sortedArray;
-  console.log(sortedArray);
-   }
-});
 
-fuck fuck fuck fuck !!!!
+  $(document).ready (function(){
+
+    $('button').click(function() {
+
+        var inputVal = $('#numInput').val();
+        // console.log(inputVal);
+      
+
+        var firstArray =inputVal.split(",");
+        // console.log(firstArray);
+
+        
+        var newArray = inputArray(firstArray);
+        // console.log(greaterArray);
+
+        var sortedArray = sortArr(greaterArray);
+        console.log("sortedArray", sortedArray);
+
+        // var sortedArray = greaterArray.sort();
+        // console.log(sortedArray);
+        // abbreviated method for sorting into an array
+
+        newString(sortedArray);
+        console.log("outputString", outputString);
+
+        $('#output').append(outputString);
+      })
+   })
